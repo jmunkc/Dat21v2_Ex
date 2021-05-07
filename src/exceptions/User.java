b2f944d78a9c8e14e73c7fabbeedbf237dd17bc0
jmunkc <jona04g8@stud.kea.dk>
2021-05-07 13:25:10 +0200

    NemID

--- src/exceptions/User.java
+++ src/exceptions/User.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class User {
@@ -38,5 +41,42 @@ public class User {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please type your password");
         setPassword(sc.nextLine());
+    }
+
+    public void createUser() throws FileNotFoundException {
+        File usersFile = new File("resources/users.csv");
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(usersFile);
+        NemIdAuthorizer aut = new NemIdAuthorizer();
+
+        pw.print("");
+
+        System.out.println(("To create a new user:"));
+
+        boolean valid = false;
+
+        while(!valid) {
+
+            System.out.println("Type your CPR number");
+            String cpr = sc.nextLine();
+
+            System.out.println("Type your new password");
+            String password = sc.nextLine();
+
+            aut.isValidInput(cpr, password);
+
+            try {
+                valid = aut.isValidPassword(cpr, password);
+
+            } catch (InvalidPasswordException e) {
+                System.out.println("Password not valid - try again");
+            }
+        }
+
+        pw.print(cpr + ";");
+        pw.println(password + ";");
+
+        pw.close();
+
     }
 }
